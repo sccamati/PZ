332de4200a0bb7b2c398d9340d287a4e5c6976b9
sccamati <64529630+sccamati@users.noreply.github.com>
2020-05-02 14:04:03 +0200

    PZ

---
+++ lab8/lab8lib/Mage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8lib
+{
+    public class Mage
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public int Strength { get; set; }
+        public int Dextirity { get; set; }
+        public int Inteligence { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public int MP { get; set; }
+        public int MaxMP { get; set; }
+        public int Damage { get; set; }
+        public int PhysicalResistance { get; set; }
+        public int FireResistance { get; set; }
+        public int IceResistance { get; set; }
+        public int PoisonResistance { get; set; }
+
+        public SpellBook SpellBook { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name }, Hp {HP}/{MaxHP}, MP {MP}/{MaxMP}, Lvl {Level}, Exp {Experience}, Str {Strength}, Dex {Dextirity}, Int {Inteligence}, Dmg {Damage}, Res [Physical {PhysicalResistance}, FIre {FireResistance}, Ice {IceResistance}, Poison {PoisonResistance}] \n{SpellBook}";
+
+        }
+    }
+}
---
+++ lab8/lab8lib/Spell.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8lib
+{
+	public enum Type
+	{
+		Offensive,
+		Defensive,
+		Healing,
+	}
+    public class Spell
+    {
+		private String name_;
+
+		public String Name
+		{
+			get { return name_; }
+			set { name_ = value; }
+		}
+
+		private Type spellType_;
+
+		public Type SpellType
+		{
+			get { return spellType_; }
+			set { spellType_ = value; }
+		}
+
+		private int cost_;
+
+		public int Cost
+		{
+			get { return cost_; }
+			set { cost_ = value; }
+		}
+
+		private int cooldown_;
+
+		public int Cooldown
+		{
+			get { return cooldown_; }
+			set { cooldown_ = value; }
+		}
+
+		private int effect_;
+
+		public int Effect
+		{
+			get { return effect_; }
+			set { effect_ = value; }
+		}
+
+		
+
+	}
+}
